54cdc29d21f88f5c55a0f2304562b1677b953ed9
Koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2022-04-03 13:41:19 +0600

    adding new folders and initilizing a project

---
+++ Web/SnickersDevOps/src/main/java/com/example/snickersdevops/SnickersDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.snickersdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnickersDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnickersDevOpsApplication.class, args);
+    }
+
+}
